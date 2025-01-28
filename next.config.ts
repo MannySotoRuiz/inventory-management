a16ede9617d4739c0c936c88a5fb9400490fd8e8
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2025-01-28 16:43:14 -0500

    initial app set up, sidebar, navbar & basic state management

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "135opydn8x.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
