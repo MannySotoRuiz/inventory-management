db3ede2d8baf5288dc335cd136b5baf7ffa5fc5f
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2025-01-29 20:03:43 -0500

    started working on dashboard metric cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+
+import CardPopularProducts from "./CardPopularProducts";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
+    </div>
+  );
 };
 
 export default Dashboard;
